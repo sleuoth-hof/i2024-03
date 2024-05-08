90378fc22672c79e39598d237109cb475f9e0de4
Vampirish <vamperish@gmail.com>
2024-05-08 08:13:21 +0200

    make function of coindesk parser

--- RSS Feed/coindesk.py
+++ RSS Feed/coindesk.py
@@ -4,27 +4,35 @@ from bs4 import BeautifulSoup
 import requests
 
 
-url = "https://www.coindesk.com/"
+def getCoindeskFeed():
+    url = "https://www.coindesk.com/"
 
-html_doc = requests.get(url)
-# response - answer. make get request to take html document of url
+    html_doc = requests.get(url)
+    # response - answer. make get request to take html document of url
 
-soup = BeautifulSoup(html_doc.text, 'html.parser')
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
 
-newses = soup.find_all(class_="static-cardstyles__StaticCardWrapper-sc-1kiw3u-0 iiwocm")
+    newses = soup.find_all(class_="static-cardstyles__StaticCardWrapper-sc-1kiw3u-0 iiwocm")
 
-# into news blocks
-for news in newses:
-    headline = news.find(class_="card-titlestyles__CardTitleWrapper-sc-1ptmy9y-0 junCw card-title-link")
-    title = headline.text.strip()
-    print("title : " + title)
-    link = url + news.a["href"]
-    print("link: " + link)
+    feed = []
 
-    # article page
-    article_html = requests.get(link)
-    article_soup = BeautifulSoup(article_html.text, 'html.parser')
-    article_content = article_soup.find(class_="contentstyle__StyledWrapper-sc-g5cdrh-0 gkcZwU composer-content")
-    article_text = article_content.text.strip()
-    # strip() - delete the big spaces
-    print("text: " + article_text + '\n')
+    # into news blocks
+    for news in newses:
+        headline = news.find(class_="card-titlestyles__CardTitleWrapper-sc-1ptmy9y-0 junCw card-title-link")
+        title = headline.text.strip()
+        print("title : " + title)
+        link = url + news.a["href"]
+        print("link: " + link)
+
+        # article page
+        article_html = requests.get(link)
+        article_soup = BeautifulSoup(article_html.text, 'html.parser')
+        article_content = article_soup.find(class_="contentstyle__StyledWrapper-sc-g5cdrh-0 gkcZwU composer-content")
+        article_text = article_content.text.strip()
+        # strip() - delete the big spaces
+        print("text: " + article_text + '\n')
+        feed += [title, link, article_text]
+    return feed
+
+
+# print(getCoindeskFeed())
