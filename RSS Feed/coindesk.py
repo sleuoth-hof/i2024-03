e74706f611206c7f2e8c02249c1adba929a16e10
Vampirish <vamperish@gmail.com>
2024-05-12 21:40:02 +0200

    Formatted date output to "%Y:%m:%d:%H:%M" form

--- RSS Feed/coindesk.py
+++ RSS Feed/coindesk.py
@@ -1,6 +1,7 @@
 # get last 6 news from coindesk.com
 
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
 
 
@@ -27,12 +28,21 @@ def getCoindeskFeed():
         # article page
         article_html = requests.get(link)
         article_soup = BeautifulSoup(article_html.text, 'html.parser')
+
+        article_date = article_soup.find(class_="typography__StyledTypography-sc-owin6q-0 iOUkmj").text.strip().replace(
+            "p.m.", "PM").replace("a.m.", "AM")
+        date_object = datetime.strptime(article_date, "%B %d, %Y at %I:%M %p UTC")
+        formatted_date = date_object.strftime("%Y:%m:%d:%H:%M")
+        print(formatted_date)
+
         article_content = article_soup.find(class_="contentstyle__StyledWrapper-sc-g5cdrh-0 gkcZwU composer-content")
         article_text = article_content.text.strip()
         # strip() - delete the big spaces
-        print("text: " + article_text + '\n')
-        feed += [title, link, article_text]
+        print("text: " + article_text)
+        print("")
+
+        feed += [title, link, formatted_date, article_text]
     return feed
 
 
-# print(getCoindeskFeed())
+print(getCoindeskFeed())
