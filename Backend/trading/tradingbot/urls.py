fe1505b9e32a958c3a58db0117093864c9d98532
Mako <btrmaks12@gmail.com>
2024-06-07 16:53:53 +0200

    Admin page

--- Backend/trading/tradingbot/urls.py
+++ Backend/trading/tradingbot/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
     path('main/',views.main, name='main'),
+    path('admin/',admin.site.urls, name='admin'),
     path('index/',views.index, name='test'),
     path("news/", views.MarketNewsView.as_view()),
     path("llm/",views.ProcessNewsView.as_view() ),
     path("cash/",views.CacheView.as_view()),
     path("cash/<int:pk>/", views.CacheView.as_view()),  # GET by ID UPDATE by ID
+    path('stockllm/', views.StockRecommendationView.as_view())
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
