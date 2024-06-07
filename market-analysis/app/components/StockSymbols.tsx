6c607de6f5d6aadda4fe5c21c55e242e699f1381
Bibimariyam <djakupovab@gmail.com>
2024-06-07 12:44:19 +0200

    Adjusted Stock Symbols section

--- market-analysis/app/components/StockSymbols.tsx
+++ market-analysis/app/components/StockSymbols.tsx
@@ -5,7 +5,7 @@ const StockSymbols: React.FC = () => {
     <section id="stocksymbols" className="min-h-screen flex flex-col items-start justify-start bg-gray-100 py-8 mt-16">
       <div className="container mx-auto px-4">
         <h2 className="text-2xl font-bold text-gray-800 mb-4">Stock Symbols</h2>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           <div className="col-span-1 lg:col-span-1 bg-white p-6 rounded-lg shadow-lg">
             <h3 className="text-xl font-bold text-gray-800 mb-4">Stocks List</h3>
             <div className="mb-4 flex">
@@ -17,9 +17,9 @@ const StockSymbols: React.FC = () => {
               <button className="ml-2 bg-gray-700 text-white px-4 py-2 rounded-lg hover:bg-gray-600">Search</button>
             </div>
           </div>
-          <div className="col-span-2 lg:col-span-2 bg-white p-6 rounded-lg shadow-lg">
+          <div className="col-span-1 lg:col-span-1 bg-white p-6 rounded-lg shadow-lg">
             <h3 className="text-xl font-bold text-gray-800 mb-4">Graphical representation</h3>
-            <div className="bg-white p-4 rounded-lg border border-gray-400 h-96">
+            <div className="bg-white p-4 rounded-lg border border-gray-400 h-96 w-96 mx-auto">
               <p>Graph placeholder</p>
             </div>
           </div>
