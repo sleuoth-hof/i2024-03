5122c29a6d5580498d66561c6d16abbd5709811c
qadrqul <jagpanthertitanium@gmail.com>
2024-06-12 12:16:19 +0200

    Added tests for index and view

--- Backend/trading/tradingbot/test_views.py
+++ Backend/trading/tradingbot/test_views.py
@@ -2,7 +2,8 @@ from django.urls import reverse
 from django.test import TestCase, Client
 from .models import CSVFile
 from .forms import CSVFileForm
-
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
 
 class IndexViewTest(TestCase):
     def setUp(self):
@@ -26,3 +27,17 @@ class IndexViewTest(TestCase):
         response = self.client.post(self.url, {'csv_file': ''})
         self.assertEqual(response.status_code, 200)
         self.assertFalse('graph_path' in response.context)
+
+class MarketNewsViewTest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('market_news_view')
+
+    def test_market_news_get(self):
+        response = self.client.get(self.url, {'category': 'general', 'minId': '10'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), MarketNews.objects.count())
+
+    def test_market_news_invalid_category(self):
+        response = self.client.get(self.url, {'category': 'invalid_category'})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
